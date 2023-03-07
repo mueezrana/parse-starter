122c6eb918738167acd9153c39d904108ad962be
mueezrana <mueezrana88@gmail.com>
2023-03-07 20:34:12 +0500

    parse stater

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { ParseProvider } from '../providers/parse/parse';
+import { AuthProvider } from '../providers/auth/auth';
+import { SideMenuComponent } from '../components/side-menu/side-menu';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    SideMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    SideMenuComponent
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ParseProvider,
+    AuthProvider
+  ]
+})
+export class AppModule {}
